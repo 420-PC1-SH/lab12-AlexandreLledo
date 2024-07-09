0e6c4223038776ef01d966c10fc0c4ab9c99fdf8
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-07-09 10:31:32 -0400

    initial commit

---
+++ processor.test.js
@@ -0,0 +1,10 @@
+const processor = require('./processor.js');
+
+describe("transmission processor", function () {
+
+    test("takes a string and returns an object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result).toEqual("object");
+    });
+
+});
